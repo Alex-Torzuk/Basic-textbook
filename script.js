2bf3e908f7af33a2b4d37e1765f2b9bed534bac9
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2024-11-05 08:55:15 +0200

    +

---
+++ script.js
@@ -0,0 +1,24 @@
+//Выделение чисел в JavaScript
+
+//parseInt - отделяет число от букв и выводит число
+
+// let a = '12px'
+// console.log(parseInt(a))
+
+
+// parseFloat - отделяет дробное число от букв 
+
+
+// let num = parseFloat('12.5px');
+// alert(num); // выведет 12.5
+
+
+
+//инкремент ++,
+
+//декремент --
+
+let num1 = 0;
+let num2 = num1++; // в переменную num2 запишется 0
+alert(num2); // выведет 0
+alert(num2)
