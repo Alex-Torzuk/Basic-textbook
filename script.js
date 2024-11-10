87de911691eb25ffd14c8e8d685b8fcddd010bc6
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2024-11-10 18:27:32 +0200

    +

--- script.js
+++ script.js
@@ -572,3 +572,15 @@
 //         console.log(i)
     
 // }
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// for(let elem of arr){
+//     console.log(elem)
+// }
+
+let obj = {x: 1, y: 2, x: 3};
+
+for(let key in obj){
+    console.log(`${key} :  ${obj[key]}`)
+}
