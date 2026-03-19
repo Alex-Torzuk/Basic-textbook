f45621187d492536a1316336f98485eade39ef01
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2026-03-19 22:32:18 +0200

    Update scriptNew.js
    9 day

--- scriptNew.js
+++ scriptNew.js
@@ -1,9 +1,12 @@
 'use strict'
 
-let num = 3;
-		
-if (num > 5 && (num < 10 || num == 20)) {
-	console.log('+++');
-} else {
-	console.log('---');
+let month = 13
+if(month > 0 && month <= 10){
+	console.log('First')
+}else if(month >=11 && month <= 20){
+	console.log('second')
+}else if(month >=21 && month <= 31){
+	console.log('three')
+}else{
+	alert('mistake')
 }
