b9deba385dbd993b1a35ff76822c79e19b41d70f
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2026-03-20 21:13:13 +0200

    Update scriptNew.js
    10 день

--- scriptNew.js
+++ scriptNew.js
@@ -1,12 +1,29 @@
 'use strict'
 
-let month = 13
-if(month > 0 && month <= 10){
-	console.log('First')
-}else if(month >=11 && month <= 20){
-	console.log('second')
-}else if(month >=21 && month <= 31){
-	console.log('three')
+// let num = 77
+
+
+// if(num >= 10 && num <= 99){
+// 	let strNum = String(num)
+// 	let sum = Number(strNum[0]) + Number(strNum[1])
+// 	console.log(sum)
+// 	if(sum <= 9){
+// 		console.log('сумма цифр однозначна')
+// 	}else{
+// 		console.log('сумма цифр двухзначна')
+// 	}
+// }
+
+
+
+// let num = 1;
+// let res = num >= 0 ? '1':'2'
+
+// console.log(res);
+
+let user = confirm('Есть ли вам уже 18 лет')
+if(user){
+	alert('сообщение с текстом для взрослых')
 }else{
-	alert('mistake')
+	alert('доступ пользователю запрещен')
 }
