b83c762b703553177f6e63fe0a35f1584c2d896b
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2026-03-31 21:30:00 +0300

    +

--- scriptNew.js
+++ scriptNew.js
@@ -16,16 +16,15 @@
 
 
 
-let arr = [
-	[1, 2, 3, 
-		[4, 5, 
-			[6, 7]
-		]
-		], 
-		[8, [
-			9, 10]
-		]
-];
+let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
+let sum = 0
 
-console.log(arr[0][0] + arr[0][1]+ arr[0][2]+arr[0][3][0]+arr[0][3][1]+arr[0][3][2][0]+arr[0][3][2][1]+arr[1][0]+arr[1][1][0]+arr[1][1][1])
+for(let i = 0; i < arr.length; i++){
+	for(let j = 0; j < arr[i].length; j++){
+		for(let k = 0; k < arr[i][j].length; k++){
+			sum+= arr[i][j][k]
+		}
+	}
+}
+console.log(sum)
