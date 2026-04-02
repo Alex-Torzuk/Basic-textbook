665c6c744bcf6a2fa6dbe1f45b2fa374633b1cff
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2026-04-02 23:18:43 +0300

    +

--- scriptNew.js
+++ scriptNew.js
@@ -16,13 +16,18 @@
 
 
 
-let arr = [];
 
-for (let i = 0; i < 3; i++) {
+let arr = []
+let a = 1
+for(let i = 0; i < 2; i++){
 	arr[i] = []
-	for (let j = 1; j <= 5; j++) {
-		arr[i].push(j);
+	for(let j = 0; j < 2; j++){
+		arr[i][j] = []
+		for(let k = 0; k < 2; k++){
+			arr[i][j].push(a++)
+		}
 	}
 }
+console.log(arr)
 
-console.log(arr);
+//[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
