5f539586a395a8152b344845accfadac84c2e4b9
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2026-06-19 20:51:42 +0300

    +

--- scriptNew.js
+++ scriptNew.js
@@ -703,15 +703,4 @@
 	//totalName.value = arrUpper.join(' ')
 	//input.dataset.min
 
-let parent = document.querySelector('#parent');
-let button = document.querySelector('button');
-let block  = document.querySelector('#block');
 
-button.addEventListener('click', function(event) {
-	block.classList.add('active');
-	event.stopPropagation();
-});
-
-parent.addEventListener('click', function() {
-	block.classList.remove('active');
-});
