513436985843562b7cd9f38948f2a3ed7f19b6fd
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2026-07-07 23:25:54 +0300

    +

--- scriptNew.js
+++ scriptNew.js
@@ -792,33 +792,14 @@
 // изучить как работаут table, tr, td, th, thead, tbody, tfoot
 //______________________
 
-let employees = [
-	{name: 'employee1', age: 30, salary: 400},
-	{name: 'employee2', age: 31, salary: 500},
-	{name: 'employee3', age: 32, salary: 600},
-];
-let table = document.getElementById('table');
+let table = document.querySelector('#table')
+let btn = document.querySelector('#btn')
+let tr = document.createElement('tr')
+btn.addEventListener('click', function(){
+	let tds = document.querySelectorAll('#table td');
 
-for (let employee of employees) {
-	let tr = document.createElement('tr');
-	
-	let td1 = document.createElement('td');
-	td1.textContent = employee.name;
-	tr.appendChild(td1);
-	
-	let td2 = document.createElement('td');
-	td2.textContent = employee.age;
-	tr.appendChild(td2);
-	td2.addEventListener('click', function() {
-		this.textContent = Number(this.textContent) + 1;
-	})
-	
-	let td3 = document.createElement('td');
-	td3.textContent = employee.salary;
-	tr.appendChild(td3);
-	td3.addEventListener('click', function() {
-		this.textContent = Number(Math.round(this.textContent * 1.1));
-	})
-	//Math.round(currentSalary * 1.1)
-	table.appendChild(tr);
+for (let td of tds) {
+	td.textContent = Number(td.textContent) * 2;
 }
+	
+})
