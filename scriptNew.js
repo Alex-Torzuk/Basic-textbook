c5fcc66e57b8a475b064a135f94ab3e24c2c29b7
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2026-08-11 22:46:48 +0300

    +

--- scriptNew.js
+++ scriptNew.js
@@ -1606,10 +1606,16 @@
 // 	});
 // }
 
-let set = new Set([1, 2, 3]);
-
-
-
-
-
-console.log(set.has( 4))
+let button = document.querySelector('button');
+let elems  = document.querySelectorAll('p');
+let set = new Set;
+for (let elem of elems) {
+	elem.addEventListener('click', function() {
+		set.add(this);
+	});
+}
+button.addEventListener('click', function() {
+	for (let elem of set) {
+		elem.textContent = elem.textContent + '!';
+	}
+});
