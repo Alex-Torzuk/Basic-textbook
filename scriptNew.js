199cfcce5f980ac353b7044e9c6462b375c3a916
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2026-09-02 20:15:54 +0300

    +

--- scriptNew.js
+++ scriptNew.js
@@ -2134,4 +2134,5 @@ localStorage.setItem('page_renews', count);
 
 // 5. Выводим результат на экран (в тело страницы)
 document.body.insertAdjacentHTML('beforeend', `<h2>Вы обновили страницу: ${count} раз(а)</h2>`);
+
 localStorage.removeItem('page_renews')
